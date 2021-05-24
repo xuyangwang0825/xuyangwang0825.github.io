bfc5302381be83c958a35ed58e5fc28f68988cfd
wangxuyang <wangxuyang@sensetime.com>
2021-05-25 00:04:16 +0800

    Site updated: 2021-05-25 00:04:16

---
+++ dist/music.js
@@ -0,0 +1,24 @@
+const ap = new APlayer({
+    container: document.getElementById('player'),
+    listFolded: false,//列表默认折叠
+    listMaxHeight: 90,//列表最大高度
+    lrcType: 3, //此为歌词格式，没有歌词可以直接删掉这一行
+    audio: [
+        {
+            name: 'name1',
+            artist: 'artist1',
+            url: 'url1.mp3',
+            cover: 'cover1.jpg',
+            lrc: 'lrc1.lrc',
+            theme: '#ebd0c2'
+        },
+        {
+            name: 'name2',
+            artist: 'artist2',
+            url: 'url2.mp3',
+            cover: 'cover2.jpg',
+            lrc: 'lrc2.lrc',
+            theme: '#46718b'
+        }
+    ]
+});

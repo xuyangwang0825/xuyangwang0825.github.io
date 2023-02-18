6759bc5b7812da5edba3a71e8dd0de755fd509b3
Xuyang Wang <wangxuya@usc.edu>
2023-02-17 23:27:34 -0800

    Site updated: 2023-02-17 23:27:34

--- dist/music.js
+++ dist/music.js
@@ -7,15 +7,22 @@ const ap = new APlayer({
         {
             name: '拝啓、少年よ',
             artist: 'Hump Back',
-            url: 'http://music.163.com/song/media/outer/url?id=864648423.mp3',
+            url: 'https://markdown.diobrando0825.cn/Hump_Back_-_%28mp3.pm%29.mp3',
             cover: '/images/Hump.jpg',
             theme: '#8A2BE2',
         },
         {
-            name: '小さな恋のうた',
-            artist: 'MONGOL800',
-            url: 'http://music.163.com/song/media/outer/url?id=22781103.mp3',
-            cover: '/images/MONGOL800.png',
+            name: 'golden hour',
+            artist: 'JVKE',
+            url: 'https://markdown.diobrando0825.cn/JVKE%20-%20golden%20hour.mp3',
+            cover: 'https://i1.sndcdn.com/artworks-izf8EJlfHMcO-0-t500x500.jpg',
+            theme: '#8A2BE2',
+        },
+        {
+            name: 'Playing God',
+            artist: 'Polyphia',
+            url: 'https://markdown.diobrando0825.cn/Polyphia%20-%20Playing%20God.mp3',
+            cover: 'https://i1.sndcdn.com/artworks-b47cUUA1beh4-0-t500x500.jpg',
             theme: '#8A2BE2',
         },
     ]
